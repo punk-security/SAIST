252bc64fd620daf4c761d9333a353a9c6742571f
SimonGurney <simon.gurney@punksecurity.co.uk>
2025-04-15 08:59:13 +0100

    fix-poem-temp (#11)

--- saist/util/poem.py
+++ saist/util/poem.py
@@ -4,7 +4,7 @@ import random
 
 
 async def poem(llm):
-    llm.model_options = {'temperature': 2.0}
+    llm.model_options = {'temperature': 1.0}
     console = Console(width=80)
     poem_type = random.choice(["haiku", "limerick","ode"", villanelle", "sonnet", "acrostic", "free verse", "ellegy", "ballad"])
     console.print(f"\nGenerating a wonderful {poem_type} poem..")
@@ -12,7 +12,7 @@ async def poem(llm):
     resp = await llm.prompt(f"""
                             Generate a short DevSecOps themed {poem_type} poem in markdown with emojis. 
                             Sign it with a made up DevSecOps themed poet name. 
-                            Do not include a title""", ""
+                            """, "Do not include a title"
                             )
     resp = resp.removeprefix("```markdown")
     resp = resp.removesuffix("```")
