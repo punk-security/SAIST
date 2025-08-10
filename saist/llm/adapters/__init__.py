03805dc5dc884bbfa7633ba90889b70eb545aad2
SimonGurney <simon.gurney@punksecurity.co.uk>
2025-08-11 00:23:56 +0100

    feat: make latex more robust (#60)
    * feat: make atex more robust
    
    * broken-adapter?
    
    * chore: pydantic deprecations
    
    * missed one

--- saist/llm/adapters/__init__.py
+++ saist/llm/adapters/__init__.py
@@ -19,17 +19,17 @@ class BaseLlmAdapter:
 
     async def prompt_structured(self, system_prompt: str, user_prompt: str, response_format: Type[BaseModel], tool_fns: Optional[List[Callable]] = None) -> BaseModel:
         tools = [Tool(fn) for fn in tool_fns] if tool_fns else []
-        agent = Agent(self.model, result_type = response_format, tools=tools, system_prompt=system_prompt)
+        agent = Agent(self.model, output_type = response_format, tools=tools, system_prompt=system_prompt)
         response = await agent.run( user_prompt=user_prompt, model_settings=self.get_model_options())
-        logger.getChild(self.__class__.__name__).debug("prompt_structured response", extra={'response_data': response.data, 'prompt': user_prompt})
-        return response.data
+        logger.getChild(self.__class__.__name__).debug("prompt_structured response", extra={'response_data': response.output, 'prompt': user_prompt})
+        return response.output
 
     async def prompt(self, system_prompt: str, user_prompt: str, tool_fns: Optional[List[Callable]] = None) -> str | None:
         tools = [Tool(fn) for fn in tool_fns] if tool_fns else []
         agent = Agent(self.model, system_prompt=system_prompt, tools=tools)
         response = await agent.run(user_prompt=user_prompt, model_settings=self.get_model_options())
-        logger.getChild(self.__class__.__name__).debug("prompt response", extra={'response_data': response.data, 'prompt': user_prompt})
-        return response.data
+        logger.getChild(self.__class__.__name__).debug("prompt response", extra={'response_data': response.output, 'prompt': user_prompt})
+        return response.output
     
     def generate_agent(self, system_prompt: str = None, tool_fns: Optional[List[Callable]] = None):
         tools = [Tool(fn) for fn in tool_fns] if tool_fns else []
