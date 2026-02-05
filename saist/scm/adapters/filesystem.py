2241aae1eb6109659563479af6ff07f86134172c
John Roberts <john.roberts@punksecurity.co.uk>
2026-02-05 15:55:45 +0000

    Autoformatting

--- saist/scm/adapters/filesystem.py
+++ saist/scm/adapters/filesystem.py
@@ -15,44 +15,66 @@ from .. import File
 
 logger = logging.getLogger(__name__)
 
+
 class FilesystemAdapter(BaseScmAdapter):
     async def get_file_contents(self, filename: str):
-        logger.debug(f"get_file_contents: reading file {filename} under {self.compare_path}")
+        logger.debug(
+            f"get_file_contents: reading file {filename} under {self.compare_path}"
+        )
         filename = os.path.join(self.compare_path, filename)
         try:
             if not pathlib.Path(filename).is_relative_to(self.compare_path):
                 raise Exception(f"Tried to access file outside of the root: {filename}")
 
-            async with aiofiles.open(filename, mode='r', encoding='utf-8') as f:
+            async with aiofiles.open(filename, mode="r", encoding="utf-8") as f:
                 contents = await f.read()
 
             return contents
         except Exception as e:
             logging.warn(f"ERR: {e}")
 
-    def __init__(self, compare_path: PathLike[AnyStr] | str, base_path: Optional[PathLike[AnyStr] | str] = None):
+    def __init__(
+        self,
+        compare_path: PathLike[AnyStr] | str,
+        base_path: Optional[PathLike[AnyStr] | str] = None,
+    ):
         self.base_path = base_path
         self.compare_path = compare_path
 
     def create_review(self, comment, review_comments, request_changes):
-        write_findings(comment,review_comments,request_changes)
+        write_findings(comment, review_comments, request_changes)
 
     def get_changed_files(self) -> list[File]:
         return list(self._iter_changed_files())
 
     def _iter_changed_files(self) -> list[File]:
-        logger.debug(f"Iterate changed files: base:{self.base_path}, compare:{self.compare_path}")
+        logger.debug(
+            f"Iterate changed files: base:{self.base_path}, compare:{self.compare_path}"
+        )
 
         if self.base_path is None:
-            diffs = [path.replace("\\","/") for path in glob.glob("**/*", root_dir=self.compare_path, recursive=True,)]
+            diffs = [
+                path.replace("\\", "/")
+                for path in glob.glob(
+                    "**/*",
+                    root_dir=self.compare_path,
+                    recursive=True,
+                )
+            ]
             logger.debug("Changed file paths", extra={"paths": list(diffs)})
 
-            for filename in [path for path in diffs if os.path.isfile(f"{self.compare_path}/{path}")]:
+            for filename in [
+                path for path in diffs if os.path.isfile(f"{self.compare_path}/{path}")
+            ]:
                 a_path = f"{self.compare_path}/{filename}"
 
                 try:
                     with open(a_path) as a:
-                        patch = "".join(difflib.unified_diff([], a.readlines(), fromfile="", tofile=f""))
+                        patch = "".join(
+                            difflib.unified_diff(
+                                [], a.readlines(), fromfile="", tofile=f""
+                            )
+                        )
 
                         yield File(filename=filename, patch=patch)
                 except UnicodeDecodeError:
@@ -69,7 +91,11 @@ class FilesystemAdapter(BaseScmAdapter):
 
             try:
                 with open(a_path, "r") as a, open(b_path, "r") as b:
-                    patch = "".join(difflib.unified_diff(a.readlines(), b.readlines(), fromfile="", tofile=""))
+                    patch = "".join(
+                        difflib.unified_diff(
+                            a.readlines(), b.readlines(), fromfile="", tofile=""
+                        )
+                    )
 
                     yield File(filename=filename, patch=patch)
             except UnicodeDecodeError:
