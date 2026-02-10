92628802642036e5a494f8bf8de452a124cd4a7b
John Roberts <john.roberts@punksecurity.co.uk>
2026-02-10 14:31:56 +0000

    Adding flag to turn off model check + blacking

--- saist/util/argparsing.py
+++ saist/util/argparsing.py
@@ -26,6 +26,7 @@ class CustomParser(argparse.ArgumentParser):
         self.print_usage()
         sys.exit(2)
 
+
 parser = CustomParser(
     usage=f"{linesep} {runtime} [options] {{ filesystem / git / github / poem }} {linesep}",
     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -51,6 +52,7 @@ Examples:{linesep}
 """,
 )
 
+
 class EnvDefault(argparse.Action):
     def __init__(self, envvar, required=True, default=None, **kwargs):
         if envvar:
@@ -58,63 +60,101 @@ class EnvDefault(argparse.Action):
                 default = environ[envvar]
         if required and default:
             required = False
-        super(EnvDefault, self).__init__(default=default, required=required, 
-                                         **kwargs)
+        super(EnvDefault, self).__init__(default=default, required=required, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, values)
 
-SCM_subparsers = parser.add_subparsers(dest="SCM", help='SCM choice',required=True)
 
-filesystem_parser = SCM_subparsers.add_parser("poem", help = "Forget code scanning, get a nice little DevSecOps poem")
+SCM_subparsers = parser.add_subparsers(dest="SCM", help="SCM choice", required=True)
+
+filesystem_parser = SCM_subparsers.add_parser(
+    "poem", help="Forget code scanning, get a nice little DevSecOps poem"
+)
 
 ### FILESYTEM
-filesystem_parser = SCM_subparsers.add_parser("filesystem", help = "Scan a local folder on disk")
+filesystem_parser = SCM_subparsers.add_parser(
+    "filesystem", help="Scan a local folder on disk"
+)
 filesystem_parser.add_argument(
-    "path", type=str, help="Path to the folder to be scanned",
-    envvar="SAIST_FILESYSTEM_PATH", action=EnvDefault
-    )
+    "path",
+    type=str,
+    help="Path to the folder to be scanned",
+    envvar="SAIST_FILESYSTEM_PATH",
+    action=EnvDefault,
+)
 filesystem_parser.add_argument(
-    "--path-for-comparison", type=str, help = "Path to generate diff with",
-    envvar="SAIST_COMPARE_PATH", action=EnvDefault, required=False
-    )
+    "--path-for-comparison",
+    type=str,
+    help="Path to generate diff with",
+    envvar="SAIST_COMPARE_PATH",
+    action=EnvDefault,
+    required=False,
+)
 
 
-### GIT 
-git_parser = SCM_subparsers.add_parser("git", help = "Scan a local git repository on disk")
+### GIT
+git_parser = SCM_subparsers.add_parser(
+    "git", help="Scan a local git repository on disk"
+)
 git_parser.add_argument(
-    "path", type=str, help="Path to the folder to be scanned",
-    envvar="SAIST_FILESYSTEM_PATH", action=EnvDefault
-    )
+    "path",
+    type=str,
+    help="Path to the folder to be scanned",
+    envvar="SAIST_FILESYSTEM_PATH",
+    action=EnvDefault,
+)
 git_parser.add_argument(
-    "--ref-for-compare", type=str, help = "Git ref to compare from",
-    envvar="SAIST_GIT_BASE_REF", action=EnvDefault, default="main"
-    )
-git_parser.add_argument("--ref-to-compare", type=str, help = "Git ref to compare to",
-    envvar="SAIST_GIT_COMPARE_REF", action=EnvDefault, default="HEAD"
-    )
+    "--ref-for-compare",
+    type=str,
+    help="Git ref to compare from",
+    envvar="SAIST_GIT_BASE_REF",
+    action=EnvDefault,
+    default="main",
+)
 git_parser.add_argument(
-    "--commit-for-compare", type=str, help = "Git commit to compare from (preferred over REF if set)",
-    envvar="SAIST_GIT_BASE_COMMIT", action=EnvDefault
-    )
-git_parser.add_argument("--commit-to-compare", type=str, help = "Git commit to compare to (preferred over REF if set)",
-    envvar="SAIST_GIT_COMPARE_COMMIT", action=EnvDefault
-    )
+    "--ref-to-compare",
+    type=str,
+    help="Git ref to compare to",
+    envvar="SAIST_GIT_COMPARE_REF",
+    action=EnvDefault,
+    default="HEAD",
+)
+git_parser.add_argument(
+    "--commit-for-compare",
+    type=str,
+    help="Git commit to compare from (preferred over REF if set)",
+    envvar="SAIST_GIT_BASE_COMMIT",
+    action=EnvDefault,
+)
+git_parser.add_argument(
+    "--commit-to-compare",
+    type=str,
+    help="Git commit to compare to (preferred over REF if set)",
+    envvar="SAIST_GIT_COMPARE_COMMIT",
+    action=EnvDefault,
+)
 
 ### GITHUB
-github_parser = SCM_subparsers.add_parser("github", help = "Scan a github PR")
+github_parser = SCM_subparsers.add_parser("github", help="Scan a github PR")
 github_parser.add_argument(
-    "repository", type=str, help="Repository",
-    envvar="GITHUB_REPOSITORY", action=EnvDefault
-    )
+    "repository",
+    type=str,
+    help="Repository",
+    envvar="GITHUB_REPOSITORY",
+    action=EnvDefault,
+)
 github_parser.add_argument(
-    "--github-token", type=str, help = "Github API token (can be set with env var GITHUB_TOKEN)",
-    envvar="GITHUB_TOKEN", action=EnvDefault, required=True
-    )
+    "--github-token",
+    type=str,
+    help="Github API token (can be set with env var GITHUB_TOKEN)",
+    envvar="GITHUB_TOKEN",
+    action=EnvDefault,
+    required=True,
+)
 github_parser.add_argument(
-    "pr", type=str, help = "Pull Request ID",
-    envvar="PR_NUMBER", action=EnvDefault
-    )
+    "pr", type=str, help="Pull Request ID", envvar="PR_NUMBER", action=EnvDefault
+)
 
 parser.add_argument(
     "--llm",
@@ -122,131 +162,220 @@ parser.add_argument(
     choices=["anthropic", "bedrock", "deepseek", "gemini", "ollama", "openai", "faike"],
     required=True,
     action=EnvDefault,
-    envvar="SAIST_LLM"
+    envvar="SAIST_LLM",
 )
 
 parser.add_argument(
-    "--llm-api-key", type=str, help = "API key for LLM (can be set with env var SAIST_LLM_API_KEY)",
-    envvar="SAIST_LLM_API_KEY", action=EnvDefault, required=False
-    )
-
-parser.add_argument(
-    "--llm-model", type=str, help = "LLM model to use, othwerise use the default",
-    envvar="SAIST_LLM_MODEL", action=EnvDefault, required=False
-    )
-
-parser.add_argument(
-    "--llm-rate-limit", help = "Max requests per second", envvar="SAIST_LLM_RATE_LIMIT", 
-    action=EnvDefault, required=False, type=int, default = 10
-    )
-
-parser.add_argument(
-    "--ollama-base-uri", type=str, help = "Base uri of ollama",
-    envvar="SAIST_OLLAMA_BASE_URI", action=EnvDefault, default = "http://localhost:11434"
-    )
-
-parser.add_argument(
-    "--openai-base-uri", type=str, help = "Base uri of openai to use any compatable service",
-    envvar="SAIST_OPENAI_BASE_URI", action=EnvDefault, required=False
-    )
-
-parser.add_argument(
-    "--interactive", help = "Spawn an interactive prompt with the LLM at the end",
-    required=False, action='store_true'
-    )
-
-parser.add_argument(
-    "--disable-tools", help="Disable usage of tools during code analysis (this is a good cost saving)",
-    required=False, action="store_true"
-    )
-
-parser.add_argument(
-    "--web", help = "Launch a web server to display findings",
-    required=False, action='store_true'
-    )
-
-parser.add_argument(
-    "--web-port", help = "Port for web server to listen on", required=False, type=int, default = 8080
-    )
-
-parser.add_argument(
-    "--web-host", type=str, help = "Host for the web server to bind to",
-    envvar="SAIST_WEB_HOST", action=EnvDefault, default = "127.0.0.1"
-    )
-
-
-parser.add_argument(
-    "--ci", help = "Exit 1 if findings identified", 
-    required=False, action='store_true', 
-    )
-
-parser.add_argument(
-    "--csv", help = "Write results to findings.csv", 
-    required=False, action='store_true', 
-    )
-
-parser.add_argument(
-    "--csv-path", type=str, help = "Path to write CSV results",
-    envvar="SAIST_CSV_PATH", action=EnvDefault, required=False, default="results.csv"
-    )
-
-parser.add_argument(
-    "--tex", help = "Write results of TeX file",
-    required=False, action='store_true'
-    )
-
-parser.add_argument(
-    "--tex-filename", type=str, help = "Filename of TeX file",
-    envvar="SAIST_TEX_FILENAME", action=EnvDefault, required=False, default="report.tex"
-    )
-
-parser.add_argument(
-    "--pdf", help = "Write results of PDF report",
-    required=False, action='store_true'
-    )
-
-parser.add_argument(
-    "--pdf-filename", type=str, help = "Filename of PDF report",
-    envvar="SAIST_PDF_FILENAME", action=EnvDefault, required=False, default="report.pdf"
-    )
-
-parser.add_argument(
-    "--disable-caching", help = "Disable local caching of results",
-      action='store_true', required=False
-    )
-
-parser.add_argument(
-    "--cache-folder", type=str, help = "Folder name for local caching",
-    envvar="SAIST_CACHE_FOLDER", action=EnvDefault, required=False, default="SAISTCache"
-    )
-
-parser.add_argument(
-    "--project-name", type=str, help = "Project name for pdf output",
-    envvar="SAIST_PROJECT_NAME", action=EnvDefault, required=False, default=None
-    )
-
-parser.add_argument(
-    "--skip-line-length-check", help = "Skip checking files for a maximum line length",
-    action='store_true', required=False
-    )
-
-parser.add_argument(
-    "--max-line-length", type=int, help = "Maximum allowed line length, files with lines longer than this value will be skipped",
-    required=False, default=1000
-    )
-
-parser.add_argument(
-    '-i', '--include', action='append', help = "Pattern or filename to explicitly include, overrides saist.ignore", nargs=1, required=False
+    "--llm-api-key",
+    type=str,
+    help="API key for LLM (can be set with env var SAIST_LLM_API_KEY)",
+    envvar="SAIST_LLM_API_KEY",
+    action=EnvDefault,
+    required=False,
 )
 
 parser.add_argument(
-    '-e', '--exclude', action='append', help = "Pattern or filename to explicitly ignore, overrides saist.include", nargs=1, required=False
+    "--llm-model",
+    type=str,
+    help="LLM model to use, othwerise use the default",
+    envvar="SAIST_LLM_MODEL",
+    action=EnvDefault,
+    required=False,
+)
+
+
+parser.add_argument(
+    "--llm-rate-limit",
+    help="Max requests per second",
+    envvar="SAIST_LLM_RATE_LIMIT",
+    action=EnvDefault,
+    required=False,
+    type=int,
+    default=10,
 )
 
 parser.add_argument(
-    "--dry-run", help = "Exit after parsing configuration and collecting files, does not perform any analysis. Useful for validating rules.",
-    action='store_true', required=False
-    )
+    "--ollama-base-uri",
+    type=str,
+    help="Base uri of ollama",
+    envvar="SAIST_OLLAMA_BASE_URI",
+    action=EnvDefault,
+    default="http://localhost:11434",
+)
+
+parser.add_argument(
+    "--openai-base-uri",
+    type=str,
+    help="Base uri of openai to use any compatable service",
+    envvar="SAIST_OPENAI_BASE_URI",
+    action=EnvDefault,
+    required=False,
+)
+
+parser.add_argument(
+    "--interactive",
+    help="Spawn an interactive prompt with the LLM at the end",
+    required=False,
+    action="store_true",
+)
+
+parser.add_argument(
+    "--disable-tools",
+    help="Disable usage of tools during code analysis (this is a good cost saving)",
+    required=False,
+    action="store_true",
+)
+
+parser.add_argument(
+    "--web",
+    help="Launch a web server to display findings",
+    required=False,
+    action="store_true",
+)
+
+parser.add_argument(
+    "--web-port",
+    help="Port for web server to listen on",
+    required=False,
+    type=int,
+    default=8080,
+)
+
+parser.add_argument(
+    "--web-host",
+    type=str,
+    help="Host for the web server to bind to",
+    envvar="SAIST_WEB_HOST",
+    action=EnvDefault,
+    default="127.0.0.1",
+)
+
+
+parser.add_argument(
+    "--ci",
+    help="Exit 1 if findings identified",
+    required=False,
+    action="store_true",
+)
+
+parser.add_argument(
+    "--csv",
+    help="Write results to findings.csv",
+    required=False,
+    action="store_true",
+)
+
+parser.add_argument(
+    "--csv-path",
+    type=str,
+    help="Path to write CSV results",
+    envvar="SAIST_CSV_PATH",
+    action=EnvDefault,
+    required=False,
+    default="results.csv",
+)
+
+parser.add_argument(
+    "--tex", help="Write results of TeX file", required=False, action="store_true"
+)
+
+parser.add_argument(
+    "--tex-filename",
+    type=str,
+    help="Filename of TeX file",
+    envvar="SAIST_TEX_FILENAME",
+    action=EnvDefault,
+    required=False,
+    default="report.tex",
+)
+
+parser.add_argument(
+    "--pdf", help="Write results of PDF report", required=False, action="store_true"
+)
+
+parser.add_argument(
+    "--pdf-filename",
+    type=str,
+    help="Filename of PDF report",
+    envvar="SAIST_PDF_FILENAME",
+    action=EnvDefault,
+    required=False,
+    default="report.pdf",
+)
+
+parser.add_argument(
+    "--disable-caching",
+    help="Disable local caching of results",
+    action="store_true",
+    required=False,
+)
+
+parser.add_argument(
+    "--cache-folder",
+    type=str,
+    help="Folder name for local caching",
+    envvar="SAIST_CACHE_FOLDER",
+    action=EnvDefault,
+    required=False,
+    default="SAISTCache",
+)
+
+parser.add_argument(
+    "--project-name",
+    type=str,
+    help="Project name for pdf output",
+    envvar="SAIST_PROJECT_NAME",
+    action=EnvDefault,
+    required=False,
+    default=None,
+)
+
+parser.add_argument(
+    "--skip-line-length-check",
+    help="Skip checking files for a maximum line length",
+    action="store_true",
+    required=False,
+)
+
+parser.add_argument(
+    "--max-line-length",
+    type=int,
+    help="Maximum allowed line length, files with lines longer than this value will be skipped",
+    required=False,
+    default=1000,
+)
+
+parser.add_argument(
+    "-i",
+    "--include",
+    action="append",
+    help="Pattern or filename to explicitly include, overrides saist.ignore",
+    nargs=1,
+    required=False,
+)
+
+parser.add_argument(
+    "-e",
+    "--exclude",
+    action="append",
+    help="Pattern or filename to explicitly ignore, overrides saist.include",
+    nargs=1,
+    required=False,
+)
+
+parser.add_argument(
+    "--dry-run",
+    help="Exit after parsing configuration and collecting files, does not perform any analysis. Useful for validating rules.",
+    action="store_true",
+    required=False,
+)
+
+parser.add_argument(
+    "--skip-llm-check",
+    help="If present, skip the LLM-based confirmation",
+    type=bool,
+    action="store_true",
+)
 
 parser.add_argument(
     "-v",
@@ -256,25 +385,36 @@ parser.add_argument(
     help="-v for verbose, -vv for extra verbose",
 )
 
+
 def parse_args():
     args = parser.parse_args()
 
     if args.llm == "bedrock" and args.llm_api_key:
-        parser.error(f"Do not provide an API key for bedrock, use AWS ENV variables https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-envvars.html")
+        parser.error(
+            f"Do not provide an API key for bedrock, use AWS ENV variables https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-envvars.html"
+        )
 
     if args.llm == "bedrock" and args.interactive:
-        parser.error("Sorry, we dont support interactive mode with bedrock as AWS tool calling is a bit broken")
+        parser.error(
+            "Sorry, we dont support interactive mode with bedrock as AWS tool calling is a bit broken"
+        )
 
-    if args.llm not in [ "ollama", "bedrock", "faike" ] and args.llm_api_key is None:
-        parser.error(f"You must provide an api key with --llm-api-key if using {args.llm}")
+    if args.llm not in ["ollama", "bedrock", "faike"] and args.llm_api_key is None:
+        parser.error(
+            f"You must provide an api key with --llm-api-key if using {args.llm}"
+        )
 
     if args.llm == "ollama" and args.interactive:
         parser.error(f"You cannot use the interactive shell with ollama currently")
 
     if args.llm == "faike" and args.interactive:
-        parser.error("Faike LLM: Certified non-existent AI doesn't support interactive mode")
-   
+        parser.error(
+            "Faike LLM: Certified non-existent AI doesn't support interactive mode"
+        )
+
     if args.pdf and which("latexmk") == None:
-        parser.error("Unable to find 'latexmk' binary in $PATH needed for PDF report building, cannot use --pdf flag")
+        parser.error(
+            "Unable to find 'latexmk' binary in $PATH needed for PDF report building, cannot use --pdf flag"
+        )
 
     return args
