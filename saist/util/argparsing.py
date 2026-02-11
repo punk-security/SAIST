cd1dfc7169ce10bbecd474e34cd455301425598c
John Roberts <john.roberts@punksecurity.co.uk>
2026-02-11 10:24:21 +0000

    Clearing up the arg docs

--- saist/util/argparsing.py
+++ saist/util/argparsing.py
@@ -372,7 +372,7 @@ parser.add_argument(
 
 parser.add_argument(
     "--disable-llm-check",
-    help="If present, skip the LLM-based confirmation",
+    help="If present, skip the second round trip to the LLM for false-positive detection.",
     action="store_true",
 )
 
