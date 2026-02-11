b87f1499cd451e922b7d0107f672f15aeb78b2ed
John Roberts <john.roberts@punksecurity.co.uk>
2026-02-11 17:35:24 +0000

    Modifying the prompt

--- saist/util/prompts.py
+++ saist/util/prompts.py
@@ -28,7 +28,7 @@ class prompts:
     CHECK_FINDING_PRE = """
     You are a senior application security engineer.
     You have been given a report on a security issue in a file from a junior security engineer.
-    Compare the report to the file, and state if the report is accurate or not.
+    Compare the report to the file, and state if the report is a false positive or a true positive.
     Provde some short feedback.
     """
     CHECK_FINDING_POST = """
